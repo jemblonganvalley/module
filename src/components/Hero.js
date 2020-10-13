a3d80b2b09f7518e6e18d41b9c74a7fedf02caca
jemblonganvalley <jemblonganvalley@gmail.com>
2020-10-13 22:38:51 +0700

    UPDATE

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -9,6 +9,7 @@ const Hero = () => {
             height : 80vh;
             display : flex;
             align-items : center;
+            justify-content : space-between;
             padding : 0 10%;
             background-image : url(https://picsum.photos/seed/${Hero.state.imgNo}/900);
             background-size : cover;
@@ -19,27 +20,75 @@ const Hero = () => {
             font-size : 4rem;
             text-shadow : 5px 5px 5px rgba(0,0,0,0.300);
             cursor : pointer;
-        " onclick="prev()">
+        " onclick="prev()" >
          keyboard_arrow_left
         </span>
+
+        <div>
+            <h1 onclick="showModal()">${Hero.state.text}</h1>
+            ${ Hero.state.showModal === true ? (
+
+                "<input placeholder='masukan judul' onchange='ubahText(event)' />"
+                
+            ) : '' }
+            
+        </div>
 
         <span class="material-icons" style="
             font-size : 4rem;
             text-shadow : 5px 5px 5px rgba(0,0,0,0.300);
             cursor : pointer;
-            margin-left : auto;
         " onclick="next()">
             keyboard_arrow_right
         </span>
-
-
 
         </section>
     `;
 };
 
 
+Hero.state = {
+    text : "UBAH ..",
+    setText : (args)=>{
+        Hero.state.text = args
+        App()
+    },
 
+    imgNo : 140,
+    imgPrev : ()=>{
+        Hero.state.imgNo -= 1
+        App()
+    },
+    imgNext : ()=>{
+        Hero.state.imgNo += 1
+        App()
+    },
+
+    showModal : false,
+    setShowModal : ()=>{
+        Hero.state.showModal = !Hero.state.showModal
+        App()
+    }
+}
+
+window.prev = ()=>{
+    Hero.state.imgPrev()
+    // console.log(Hero.state.imgNo)
+}
+
+window.next = ()=>{
+    Hero.state.imgNext()
+    // console.log(Hero.state.imgNo)
+}
+
+window.showModal = ()=>{
+    Hero.state.setShowModal()
+    console.log(Hero.state.showModal)
+}
+
+window.ubahText = (event)=>{
+    Hero.state.setText(event.target.value)
+}
 
 //export Hero agar bisa digunakan di file lain
 export default Hero;
